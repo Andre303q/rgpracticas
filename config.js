51d51b93c3b0305be55efd8cfd928a8d4b0c489b
Andre303q <graciabonillasebastianandre@gmail.com>
2026-09-03 16:37:17 -0600

    primer commit

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+export const env = {
+    FIREBASE_API_KEY: "AIzaSyB6KT4Gw-O4NDhsAREAeAdAmrIuMH53ZyQ",
+    FIREBASE_AUTH_DOMAIN: "rgpracticantes-da4bc.firebaseapp.com",
+    FIREBASE_PROJECT_ID: "rgpracticantes-da4bc",
+    FIREBASE_STORAGE_BUCKET: "rgpracticantes-da4bc.firebasestorage.app",
+    FIREBASE_MESSAGING_SENDER_ID: "378103051147",
+    FIREBASE_APP_ID: "1:378103051147:web:0c30387cc661d75e0ff58e"
+};
